dfb22e327b781589de304ba7735d0a3297702ec1
Yingting Yang <yangy389@newschool.edu>
2016-09-08 16:07:20 -0400

    progress

--- Week Two clog Tuesday/Assets/norotate.cs
+++ Week Two clog Tuesday/Assets/norotate.cs
@@ -1,12 +1,19 @@
  using UnityEngine;
- public class CameraFollow : MonoBehaviour
+ public class norotate : MonoBehaviour
  {
      public Transform Player;
      public Vector3 Offset;
+     private Quaternion startRot;
+
+     void Start() {
+     	startRot = transform.rotation;
+     }
+
  
      void LateUpdate ()
+     //http://answers.unity3d.com/questions/342550/camera-follows-rotation-how-can-i-fix-it.html
      {
          if (Player != null)
-             transform.rotation = Quaternion.Euler(Vector3.zero);
+             transform.rotation = startRot;//Quaternion.Euler(Vector3.zero);
      }
  }
