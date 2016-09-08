ec9bd32620cfb9ea5ac97e45e240230568c341cc
Yingting Yang <yangy389@newschool.edu>
2016-09-08 18:20:25 -0400

    week two thur class

---
+++ Week Two clog Tuesday/Assets/TrapDoorStwich.cs
@@ -0,0 +1,21 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class TrapDoorStwich : MonoBehaviour {
+   float rotationspeed = 5;
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	   //if I hit a specific key, open the trap door
+		 if (Input.GetKey( KeyCode.T )){
+		  print("open trapdoor!");
+            // rotatationspeed z to -90degree
+		  transform.Rotate(Vector3.forward * rotationspeed);
+		 }
+	}
+}
---
+++ Week Two clog Tuesday/Assets/movewithforce.cs
@@ -0,0 +1,22 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class movewithforce : MonoBehaviour {
+
+       float acceleration = 5;
+       Rigidbody thisRigidBody;
+
+	// Use this for initialization
+		void Start () {
+			thisRigidBody =GetComponent<Rigidbody>();
+		 }
+	
+	// Update is called once per frame
+	   void Update () {
+	//Debug.Log( Vector3.forward);
+	       Debug.Log( Input.GetAxis("Horizontal") );
+	       thisRigidBody.AddForce(Vector3.right * Input.GetAxis("Horizontal")* acceleration);
+	       thisRigidBody.AddForce(Vector3.up * Input.GetAxis("Vertical")* acceleration);
+	//     GetComponent<Rigidbody>().AddForce( Vector3.right * Input.GetAxis("Horizontal")*acceleration);
+	 } 
+}
